8da6cc95abcf22ba200c0baa46e26195225a3692
Shiyu Wang <42013343+shi-yu-wang@users.noreply.github.com>
2020-10-23 18:56:19 -0400

    Create PeakIndexInAMountainArray_Template.py

---
+++ Python/PeakIndexInAMountainArray_Template.py
@@ -0,0 +1,26 @@
+class Solution:
+    """
+    @param nums: a mountain sequence which increase firstly and then decrease
+    @return: then mountain top
+    """
+    def mountainSequence(self, nums):
+        # write your code here
+        if not nums:
+            return -1
+        
+        start, end = 0, len(nums) - 1
+        
+        while start + 1 < end:
+            mid = start + (end - start) // 2
+            if nums[mid] < nums[mid + 1]:
+                start = mid
+            else:
+                end = mid
+        
+        if nums[start] > nums[end]:
+            return nums[start]
+        if nums[end] > nums[start]:
+            return nums[end]
+
+        return -1                
+                
