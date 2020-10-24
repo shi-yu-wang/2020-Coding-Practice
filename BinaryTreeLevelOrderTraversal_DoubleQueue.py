83ec6ac783c16e1dff7416f5afb768095794006b
Shiyu Wang <42013343+shi-yu-wang@users.noreply.github.com>
2020-10-24 15:59:54 -0400

    Create BinaryTreeLevelOrderTraversal_DoubleQueue.py

---
+++ BinaryTreeLevelOrderTraversal_DoubleQueue.py
@@ -0,0 +1,32 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: A Tree
+    @return: Level order a list of lists of integer
+    """
+    def levelOrder(self, root):
+        # write your code here
+        if not root:
+            return []
+        
+        queue = [root]
+        results = []
+        
+        while queue:
+            next_queue = []
+            results.append([node.val for node in queue])
+            for node in queue:
+                if node.left:
+                    next_queue.append(node.left)
+                if node.right:
+                    next_queue.append(node.right)
+            queue = next_queue
+        
+        return results
