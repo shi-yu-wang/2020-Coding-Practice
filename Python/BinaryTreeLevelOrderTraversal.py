c21bb49ddb8d26b8440b1c1fb117a998c79f63dc
Shiyu Wang <42013343+shi-yu-wang@users.noreply.github.com>
2020-10-24 14:56:49 -0400

    Create BinaryTreeLevelOrderTraversal.py

---
+++ Python/BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,32 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: A Tree
+    @return: Level order a list of lists of integer
+    """
+    def levelOrder(self, root):
+        # write your code here
+        if not root:
+            return []
+            
+        queue = collections.deque([root])
+        result = []
+        
+        while queue:
+            result.append([node.val for node in queue])
+            
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        
+        return result
