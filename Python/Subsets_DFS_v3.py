79e6c7debedf384f6e4d6780d772b59b8f284cbd
Shiyu Wang <42013343+shi-yu-wang@users.noreply.github.com>
2020-10-31 13:49:24 -0400

    Create Subsets_DFS_v3.py

---
+++ Python/Subsets_DFS_v3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        if not nums:
+            return results
+        
+        nums.sort()
+        self.dfs(nums, 0, [], results)
+        return results
+    
+    def dfs(self, nums, startIndex, subset, results):
+        results.append(subset[:])
+        
+        for i in range(startIndex, len(nums)):
+            subset = subset + [nums[i]]
+            self.dfs(nums, i + 1, subset, results)
+            del subset[len(subset) - 1]
